853ea657d5ac1c002103528d9a124e8bd05506cd
crazycattle <2359590507@qq.com>
2021-08-25 08:03:03 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: "module", // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+  ],
+  rules: {},
+};
---
+++ .prettierrc.js
@@ -0,0 +1,20 @@
+// 具体配置可以参考 https://prettier.io/docs/en/options.html
+module.exports = {
+  printWidth: 100,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false, // 未尾逗号
+  singleQuote: true, // 单引号
+  trailingComma: 'none', // 未尾分号
+  vueIndentScriptAndStyle: true,
+  quoteProps: 'as-needed',
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  arrowParens: 'always',
+  insertPragma: false,
+  requirePragma: false,
+  proseWrap: 'never',
+  htmlWhitespaceSensitivity: 'strict',
+  endOfLine: 'lf'
+};
