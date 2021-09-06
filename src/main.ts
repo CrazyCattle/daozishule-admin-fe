4d257c3ca16166737f7e40b746f2cfaf93434fdd
crazycattle <2359590507@qq.com>
2021-09-06 14:55:42 +0800

    add: depend

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,16 @@ if (token && _id) {
   axios.defaults.headers['_id'] = _id
 }
 
+import VMdEditor from '@kangc/v-md-editor'
+import VMdPreview from '@kangc/v-md-editor/lib/preview'
+import '@kangc/v-md-editor/lib/style/base-editor.css'
+import '@kangc/v-md-editor/lib/style/preview.css'
+import vuepressTheme from '@kangc/v-md-editor/lib/theme/vuepress.js'
+import '@kangc/v-md-editor/lib/theme/style/vuepress.css'
+import Prism from 'prismjs'
+import 'prismjs/components/prism-json'
+VMdPreview.use(vuepressTheme, { Prism })
+VMdEditor.use(vuepressTheme, { Prism })
+
 const app = createApp(App)
-app.use(router)
-app.mount('#app')
+app.use(router).use(VMdPreview).use(VMdEditor).mount('#app')
